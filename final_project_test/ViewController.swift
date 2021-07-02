d63d78f4a3248669c0928eba56371b8e20a46d7f
Tatiana <tanyaozerskaya@gmail.com>
2021-07-02 17:42:43 +0300

    added question class and array.

--- final_project_test/ViewController.swift
+++ final_project_test/ViewController.swift
@@ -9,11 +9,19 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    var questions = [Question]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+        fillData()
     }
 
+    func fillData(){
+        questions.append(Question(question: "What is Earth's largest continent?", answers: ["Asia", "Antarctica", "Africa", "Europe"], correctAnswerIndex: 0))
+        
+        //here will be added other 9 questions
+    }
 
 }
 
